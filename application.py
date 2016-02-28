64c3a1fadb8d28552aae2dc2f1bacde39bdd7261
Daniel Ng <daniel@medium-one.com>
2016-02-28 03:02:38 -0800

    refactor, less parsing

--- application.py
+++ application.py
@@ -1,17 +1,21 @@
-from flask import Flask, request, Response, stream_with_context, render_template, jsonify
-from tweepy.streaming import StreamListener
+from flask import Flask, request, Response, render_template
+from listener import TweetStreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-from listener import StreamListener
-import simplejson as json
-import os
 import redis
+import os
 
 application = Flask(__name__)
-red = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+access_token = os.environ.get('TWITTER_ACCESS_TOKEN') 
+access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET') 
+consumer_key = os.environ.get('TWITTER_CONSUMER_KEY') 
+consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET') 
+
+r = redis.StrictRedis(host=os.getenv('REDIS_HOST'), port=6379, db=0)
 
 def event_stream():
-    queue = red.pubsub()
+    queue = r.pubsub()
     queue.subscribe('tweet_stream')
     for message in queue.listen():
         yield 'data: %s\n\n' % message['data']
@@ -25,5 +29,11 @@ def get_tweets():
     return Response(event_stream(), mimetype="text/event-stream")
 
 if __name__ == '__main__':
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    listener = TweetStreamListener()
+    stream = Stream(auth, listener)
+    stream.filter(locations=[-125.0011, 24.9493, -66.9326, 49.5904], async=True)
+
     application.debug = True
     application.run(threaded=True)
