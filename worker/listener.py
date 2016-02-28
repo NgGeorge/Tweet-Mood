6c77f84a4f4381ac5ac2cdeff098196815efbd61
Daniel Ng <danielng@uw.edu>
2016-02-28 14:37:45 +0000

    classify incoming tweets

--- worker/listener.py
+++ worker/listener.py
@@ -1,18 +1,25 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+from classifier.tweetClassifier import tweetClassifier
 import simplejson as json
 import redis
 import os
 
 r = redis.StrictRedis(host=os.getenv('REDIS_HOST'), port=6379, db=0)
+clf = tweetClassifier()
 
 class TweetStreamListener(StreamListener):
 
+    with open('classifier/save_trained.json', 'r') as model:
+        clf.from_json(model.read())
+
     def on_data(self, data):
         tweet = json.loads(data)
-        print tweet['text']
-        r.publish('tweet_stream', json.dumps(tweet))
+	if tweet.get('text', None) and tweet.get('place', {}).get('country_code', None) == 'US':
+	    tweet['score'] =  clf.classify(tweet['text'])
+	    if tweet['score'] != 'neutral':
+	        r.publish('tweet_stream', json.dumps(tweet))
         return True
 
     def on_error(self, status):
--- worker/stream.py
+++ worker/stream.py
@@ -1,5 +1,5 @@
 import daemon
-from listener import TweetStreamListener
+from worker.listener import TweetStreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import redis
@@ -11,12 +11,16 @@ def stream():
 	consumer_key = os.environ.get('TWITTER_CONSUMER_KEY') 
 	consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET') 
 
-	r = redis.StrictRedis(host=os.getenv('REDIS_HOST'), port=6379, db=0)
 	auth = OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_token_secret)
+
+	r = redis.StrictRedis(host=os.getenv('REDIS_HOST'), port=6379, db=0)
+
 	listener = TweetStreamListener()
+
 	stream = Stream(auth, listener)
 	stream.filter(locations=[-125.0011, 24.9493, -66.9326, 49.5904], async=True)
 
-with daemon.DaemonContext():
-	stream()
+stream()
+#with daemon.DaemonContext():
+#	stream()
